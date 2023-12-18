3db59adec9ce1bd04e41fee9a90b060bffd84954
Mayank <mail.mayank001@gmail.com>
2023-12-19 01:34:32 +0530

    Input

---
+++ 2_Input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; // We define std for all cout.
+
+int main(){
+    int x;
+    cout <<"Enter the input: ";
+    cin >> x;     //cin in used for taking input from the user.
+    cout << "Value of x : " << x << endl;
+    return 0;
+}
