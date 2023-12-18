537df29baaffa6dab6ec5efef59d5fa22601e798
Mayank <mail.mayank001@gmail.com>
2023-12-19 01:46:31 +0530

    Datatype

---
+++ 3_Datatype.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> //This contains all the library in c++.
+using namespace std;
+
+int main()
+{
+    // int contains limited range of datatype to store.
+    int x = 10, y;
+
+    // long contains wider range of datatype to store.
+    cin >> y;
+    cout << y << endl;
+
+    // long long contains much wider range of datatype to store.
+    long long z = 15000000000;
+    cout << z;
+
+    // Float,double
+    float a = 5.82;
+    cout << endl << a;
+
+    return 0;
+}
