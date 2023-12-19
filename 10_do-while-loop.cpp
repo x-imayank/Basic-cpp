bb1490a0b2353cf5cdcdf7f11f767a4c4ca3dfea
Mayank <mail.mayank001@gmail.com>
2023-12-19 19:09:07 +0530

    Upload

---
+++ 10_do-while-loop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    do
+    {
+        cout << i << endl;
+        i = i + 1;
+    } 
+    while (i <= 10);
+
+    return 0;
+}
---
+++ 8_For-loop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 1; i <= 10; i++) // Printing value 10 times.
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
