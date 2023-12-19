2ceb3bb90d0c70073acbe1dd8f25de71869db364
Mayank <mail.mayank001@gmail.com>
2023-12-19 16:22:19 +0530

    update

--- 3_Datatype.cpp
+++ 3_Datatype.cpp
@@ -1,22 +1,38 @@
-#include <bits/stdc++.h> //This contains all the library in c++.
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    // int contains limited range of datatype to store.
-    int x = 10, y;
+    // int ,long, long long, float, double
+    // string and getline
+    // char
 
-    // long contains wider range of datatype to store.
+    int x = 10; // int contains limited range of datatype to store.
+
+    long y; // long contains wider range of datatype to store.
     cin >> y;
     cout << y << endl;
 
-    // long long contains much wider range of datatype to store.
-    long long z = 15000000000;
+    long long z = 15000000000; // long long contains much wider range of datatype to store.
     cout << z;
 
     // Float,double
     float a = 5.82;
-    cout << endl << a;
+    cout << a;
+    
+
+    string s1, s2;
+    cout << "Enter the Sentence";
+    cin >> s1;                       // Input from user in s1.
+    cin >> s2;                       // Input from user in s2.
+    cout << s1 << " " << s2 << endl; // This will exit the s1 and s2 with containing the space.
+
+    string str;
+    getline(cin, str); // This is for taking all the word in a line.
+    cout << str;
+
+    char ch = 'H'; // It takes single character of a word.
+    cout << ch;
 
     return 0;
 }
