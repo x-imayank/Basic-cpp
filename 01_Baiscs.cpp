502b1668ad8fd94216f30afb96430a38f3828816
Mayank <mail.mayank001@gmail.com>
2024-01-10 00:57:26 +0530

    First commit

---
+++ 01_Baiscs.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Header file it helps in input and output.
+
+int main() // Main is the entry point.
+{
+    std::cout << "Hello world";
+    return 0;
+}
--- 01_Print.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream> //Iostream is the library of c++ language.
-
-int main(){
-    std::cout<<"Hello world!"; //In c++ cout is used for printing something.
-    // std is the namespace.
-    return 0;
-}
