b043f69e5ad990d11cbdde416e751c06b0c26ef2
Mayank <mail.mayank001@gmail.com>
2024-01-11 02:44:48 +0530

    First commit

---
+++ 10_Switch/01_Switch.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char vowel;
+    cout << "Enter the vowel: ";
+    cin >> vowel;
+
+    switch (vowel)
+    {
+    case 'a':
+        cout << "Vowel.";
+        break;
+    case 'e':
+        cout << "Vowel.";
+        break;
+    case 'i':
+        cout << "Vowel.";
+        break;
+    case 'o':
+        cout << "Vowel.";
+        break;
+    case 'u':
+        cout << "Vowel.";
+        break;
+    case 'A':
+        cout << "Vowel.";
+        break;
+    case 'E':
+        cout << "Vowel.";
+        break;
+    case 'I':
+        cout << "Vowel.";
+        break;
+    case 'O':
+        cout << "Vowel.";
+        break;
+    case 'U':
+        cout << "Vowel.";
+        break;
+    default:
+        cout << "Enter correct word.";
+        break;q
+    }
+}
