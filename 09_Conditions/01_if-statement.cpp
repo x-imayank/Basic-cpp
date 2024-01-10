8391b46bf15aa6091f76ebc83cb6e806d90fd209
Mayank <mail.mayank001@gmail.com>
2024-01-11 02:45:40 +0530

    First commit

---
+++ 09_Conditions/01_if-statement.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 4;
+    int b = 3;
+
+    if (a == b)
+    {
+        cout << "true";
+    }
+    else
+    {
+        cout << "false";
+    }
+}
