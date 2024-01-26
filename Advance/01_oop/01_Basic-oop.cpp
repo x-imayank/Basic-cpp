8dfec928dfd47b98006b96fc2e5092bfc7197a6b
Mayank <mail.mayank001@gmail.com>
2024-01-26 21:01:37 +0530

    First commit

--- Advance/01_oop/01_Basic-oop.cpp
+++ Advance/01_oop/01_Basic-oop.cpp
@@ -9,12 +9,33 @@ public:
    string mySting;
 };
 
+class Cars
+{
+public:
+   string brand;
+   int year;
+};
+
 int main()
 {
+   // First class start
    myClasses digits;
    digits.myNum = 12;
    digits.mySting = "CPP";
 
    cout << digits.myNum << endl;
    cout << digits.mySting << endl;
+
+
+   // Second Class start
+   Cars obj1;
+   obj1.brand = "BMW";
+   obj1.year = 1984;
+
+   Cars obj2;
+   obj2.brand = "Volvo";
+   obj2.year = 2007;
+
+   cout << obj1.brand << "\t" << obj1.year << endl;
+   cout << obj2.brand << "\t" << obj2.year << endl;
 }
