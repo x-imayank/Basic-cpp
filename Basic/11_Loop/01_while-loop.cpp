5240e1539ec0b00c77cd5ccd981bc3b32e3cb9fa
Mayank <mail.mayank001@gmail.com>
2024-01-30 18:53:21 +0530

    Update

---
+++ Basic/11_Loop/01_while-loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a, i = 1, sum = 0;
+
+   cout << "Enter the last digit: ";
+   cin >> a;
+
+   while (i <= a)
+   {
+      sum = sum + i;
+      i = i + 1;
+   }
+
+   cout << sum;
+   return 0;
+}
